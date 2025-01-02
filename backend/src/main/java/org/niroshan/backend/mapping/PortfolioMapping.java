a403977de020ff7806bd775e71af99740c2267bf
Niroshan <niroshan9812@gmail.com>
2025-01-02 11:08:33 +0530

    Portfolio and DTO mapping

---
+++ backend/src/main/java/org/niroshan/backend/mapping/PortfolioMapping.java
@@ -0,0 +1,23 @@
+package org.niroshan.backend.mapping;
+
+import org.niroshan.backend.Model.Portfolio;
+import org.niroshan.backend.dto.PotfolioDto;
+
+public class PortfolioMapping {
+    public static Portfolio maptoPortfolio(PotfolioDto potfolioDto){
+        return  new Portfolio(
+                potfolioDto.getId(),
+                potfolioDto.getShortDescription(),
+                potfolioDto.getDescription(),
+                potfolioDto.getGithubLink()
+        );
+    }
+    public static PotfolioDto maptoPotfolioDto(Portfolio portfolio){
+        return new PotfolioDto(
+                portfolio.getId(),
+                portfolio.getShortDescription(),
+                portfolio.getDescription(),
+                portfolio.getGithubLink()
+        );
+    }
+}
